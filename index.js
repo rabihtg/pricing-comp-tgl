3506269c934542e911bc360777f9c3b312b278a4
rabih harb <rabiharb.ss@outlook.com>
2022-02-17 21:55:22 +0200

    initial, final draft one

---
+++ index.js
@@ -0,0 +1,40 @@
+const slider = document.querySelector(".slider");
+const sliderBox = document.querySelector(".slider-btn");
+const annualData = document.querySelectorAll(".card__price.annual");
+const monthlyData = document.querySelectorAll(".card__price.monthly");
+
+slider.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    if (sliderBox.checked === true) {
+      sliderBox.checked = false;
+    } else {
+      sliderBox.checked = true;
+    }
+  }
+});
+
+sliderBox.addEventListener("change", () => {
+  toggleData();
+  localStorage.toggledPlan = sliderBox.checked;
+});
+
+window.addEventListener("load", () => {
+  if (localStorage.toggledPlan === "true") {
+    sliderBox.setAttribute("checked", true);
+  } else {
+    sliderBox.removeAttribute("checked");
+    toggleData();
+  }
+});
+
+function toggleData() {
+  annualData.forEach((item) => {
+    item.classList.toggle("hidden-data");
+  });
+  document.querySelectorAll(".main__card").forEach((item) => {
+    item.classList.toggle("ann");
+  });
+  monthlyData.forEach((item) => {
+    item.classList.toggle("hidden-data");
+  });
+}
